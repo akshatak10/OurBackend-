09e6f67935394ccbceb62deb788f4583bf065ab2
Akshat Rawat <akshatrawatkv10@gmail.com>
2025-05-07 04:39:47 +0530

    add getWatchHistory function to retrieve user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -415,7 +415,59 @@ const getUserChannelProfile = asyncHandler( async (req, res) => {
 })
 
 
+const getWatchHistory = asyncHandler( async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                // _id : req.user._id // this will not work because mongoose doesn't work here code of ap passeson directly
+                _id : new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [ // Nested Pipelines
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]                   
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first : "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
 
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, user[0].watchHistory, "watchhistory fetched successfully"
+        )
+    )
+
+})
 
 
 export {
@@ -428,5 +480,6 @@ export {
     updateAccountDetails,
     updateCoverImage,
     updateAvatar,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
